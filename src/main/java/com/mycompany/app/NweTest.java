331bc988f2fc92350f3f75acbe8ebd33f0698904
unknown <nikita.p.raikar@gmail.com>
2018-09-28 13:34:04 -0500

    Test

---
+++ src/main/java/com/mycompany/app/NweTest.java
@@ -0,0 +1,19 @@
+package com.mycompany.app;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NweTest {
+	 @Test
+	  public void f() {
+			
+			System.setProperty("webdriver.chrome.driver",
+					"A://software//selenium//chromedriver_win32//chromedriver.exe");
+			WebDriver dir = new FirefoxDriver();
+
+			dir.get("http://demo.automationtesting.in/FileUpload.html");
+
+	  }
+
+}
